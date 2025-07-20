d923b32bf5663b18b92a9cbd662b0bcd89220624
Satyajit Mishra <143812895+satyajitmishra-dev@users.noreply.github.com>
2025-07-20 10:25:44 +0530

    Add Contact Form Submission Feature

--- src/js/script.js
+++ src/js/script.js
@@ -139,3 +139,35 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 /******************************* Contact Form *************************************** */
 
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('contactForm');
+    const status = document.getElementById('form-status');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                status.innerHTML = "Thanks for your submission!";
+                form.reset();
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                    } else {
+                        status.innerHTML = "Oops! There was a problem submitting your form";
+                    }
+                });
+            }
+        }).catch(error => {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+        });
+    }
+    form.addEventListener("submit", handleSubmit);
+});
